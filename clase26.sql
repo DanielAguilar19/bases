946e46d52ca93638097fc16784f525d7af145176
DanielAguilar19 <98502062+DanielAguilar19@users.noreply.github.com>
2023-10-26 07:54:07 -0600

    clase del 26 se trabajo con la base de datos del archivo .bak

---
+++ clase26.sql
@@ -0,0 +1,5 @@
+---queries de la base de datos del campus
+
+--select empleadoID, nombre_empleadoID, horas_extras, (horas extras(200) as total_extras, (sueldo + (horas_extras*200)) as total
+
+select codigo_empleado, sueldo, horas_extras , total_extras, total,
