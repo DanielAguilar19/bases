2ff511281f74b697b9b6f55ddb94ab5dcce1629e
DanielAguilar19 <98502062+DanielAguilar19@users.noreply.github.com>
2023-10-31 17:56:52 -0600

    Update SQLQuery3.sql

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,5 +1,7 @@
---SELECT * FROM VEHICULO where(versionID='2') and (ano_ingreso between '2003-01-01' AND '2010-12-31')
+--1) SELECT * FROM VEHICULO where(versionID='2') and (ano_ingreso between '2003-01-01' AND '2010-12-31')
 
---SELECT COUNT(*) AS SERVICIOS_POR_VEHICULO FROM ORDEN_SERVICIO AS s  inner join  VEHICULO AS v ON s.vehiculoID = v.vehiculoID  WHERE v.marcaID='1'or v.marcaID='4'  AND s.fecha_orden BETWEEN '2008-01-01' AND '2009-12-31';
+--2) SELECT COUNT(*) AS SERVICIOS_POR_VEHICULO FROM ORDEN_SERVICIO AS s  inner join  VEHICULO AS v ON s.vehiculoID = v.vehiculoID  WHERE v.marcaID='1'or v.marcaID='4'  AND s.fecha_orden BETWEEN '2008-01-01' AND '2009-12-31';
 
---JUAN= ID 1 ::::: SEBASTIAN= ID 2
+--JUAN = ID 1 --------- SEBASTIAN = ID 2
+
+--4) select COUNT(*) AS CARROS_JUAN from ORDEN_SERVICIO where (empleadoID='1')
